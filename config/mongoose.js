c44a0c2d6c15c3497c0395f0020adddd3ec28eac
williamsjohn <williamsjohn2018@gmail.com>
2019-02-06 14:30:27 +0100

    ezzy correct

--- config/mongoose.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require ('mongoose');
-mongoose.connect('mongodb://Daveux:D@veuxbap@55w0rdlm67...@ds223015.mlab.com:23015/payments',{useNewUrlParser:true});
-
-module.exports = {mongoose}
